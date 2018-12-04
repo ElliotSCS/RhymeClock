2722979620a734e8c9bf4c8f5f10b17f2f5d36bc
ElliotSCS <edsegal2@illinois.edu>
2018-12-04 16:04:23 -0600

    Alarm creation page is done

--- Clocky/Clocky/app/src/main/java/com/example/dorian/clocky/MainActivity.java
+++ Clocky/Clocky/app/src/main/java/com/example/dorian/clocky/MainActivity.java
@@ -2,6 +2,11 @@ package com.example.dorian.clocky;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TimePicker;
+import android.widget.ToggleButton;
+
+import java.sql.SQLOutput;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,66 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void createAlarm(View activity_main) {
+        setContentView(R.layout.alarmcreation);
+    }
+    public void cancelMethod(View alarmcreation) {
+        setContentView(R.layout.activity_main);
+    }
+    public void saveMethod(View alarmcreation) {
+        int[] days = new int[7];
+        ToggleButton mon = (ToggleButton) findViewById(R.id.mondayButton);
+        ToggleButton tues = (ToggleButton) findViewById(R.id.tuesdayButton);
+        ToggleButton wed = (ToggleButton) findViewById(R.id.wednesdayButton);
+        ToggleButton thur = (ToggleButton) findViewById(R.id.thursdayButton);
+        ToggleButton fri = (ToggleButton) findViewById(R.id.fridayButton);
+        ToggleButton sat = (ToggleButton) findViewById(R.id.saturdayButton);
+        ToggleButton sun = (ToggleButton) findViewById(R.id.sundayButton);
+        if (mon.isChecked()) {
+            days[0] = 1;
+        } else {
+            days[0] = 0;
+        }
+        if (tues.isChecked()) {
+            days[1] = 1;
+        } else {
+            days[1] = 0;
+        }
+        if (wed.isChecked()) {
+            days[2] = 1;
+        } else {
+            days[2] = 0;
+        }
+        if (thur.isChecked()) {
+            days[3] = 1;
+        } else {
+            days[3] = 0;
+        }
+        if (fri.isChecked()) {
+            days[4] = 1;
+        } else {
+            days[4] = 0;
+        }
+        if (sat.isChecked()) {
+            days[5] = 1;
+        } else {
+            days[5] = 0;
+        }
+        if (sun.isChecked()) {
+            days[6] = 1;
+        } else {
+            days[6] = 0;
+        }
+        TimePicker time = findViewById(R.id.simpleTimePicker);
+        int hour = time.getCurrentHour();
+        int minute = time.getCurrentMinute();
+        String amOrPm = "am";
+        if (hour == 12) {
+            amOrPm = "pm";
+        } else if (hour > 12) {
+            hour = hour - 12;
+            amOrPm = "pm";
+        }
     }
 }
