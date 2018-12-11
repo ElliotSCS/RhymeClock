b37677b52d2f32c6d284f17bb7e3cf6c51b7afd6
dorianisnairod <rubends2@illinois.edu>
2018-12-11 16:09:08 -0600

    Implement demo
    changed sounds to demo

---
+++ Clocky/Clocky/app/src/main/java/com/example/dorian/clocky/RhymeMinigame.java
@@ -0,0 +1,14 @@
+package com.example.dorian.clocky;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+import android.os.Bundle;
+import android.view.View;
+
+public class RhymeMinigame extends AppCompatActivity {
+    public void sendRhyme() {
+        Toast.makeText(RhymeMinigame.this, "This is my Toast message!",
+                Toast.LENGTH_LONG).show();
+        return;
+    }
+}
