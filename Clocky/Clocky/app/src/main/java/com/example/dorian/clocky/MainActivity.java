368b50bdb134b974ac34772e6069b092bcd00d57
ElliotSCS <edsegal2@illinois.edu>
2018-12-11 16:08:16 -0600

    First Alarm works

--- Clocky/Clocky/app/src/main/java/com/example/dorian/clocky/MainActivity.java
+++ Clocky/Clocky/app/src/main/java/com/example/dorian/clocky/MainActivity.java
@@ -1,15 +1,16 @@
 package com.example.dorian.clocky;
 
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.TimePicker;
 import android.widget.ToggleButton;
-
-import java.sql.SQLOutput;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
-
+    public boolean firstAlarm = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -80,16 +81,57 @@ public class MainActivity extends AppCompatActivity {
         TimePicker time = findViewById(R.id.simpleTimePicker);
         int hour = time.getCurrentHour();
         int minute = time.getCurrentMinute();
-        String amOrPm = "am";
+        String amOrPm = "AM";
         if (hour == 12) {
-            amOrPm = "pm";
+            amOrPm = "PM";
         } else if (hour > 12) {
             hour = hour - 12;
-            amOrPm = "pm";
+            amOrPm = "PM";
         }
         setContentView(R.layout.activity_main);
+        alarmPanel(days, hour, minute, amOrPm);
     }
-    public void alarmPanel() {
-
+    public void alarmPanel(int[] days, int hour, int minute, String AP) {
+        ConstraintLayout sample = findViewById(R.id.Sample_Alarm);
+        if (firstAlarm) {
+            final TextView thing = findViewById(R.id.alarmTime);
+            String fack = "test";
+            thing.setText(hour + ":" + minute + " " + AP);
+            //hour + ":" + minute + " " + AP
+            Switch blah = findViewById(R.id.OnOffSwitch);
+            blah.setChecked(true);
+            if (days[0] == 1) {
+                ToggleButton mon = findViewById(R.id.MondayToggle);
+                mon.setChecked(true);
+            }
+            if (days[1] == 1) {
+                ToggleButton tues = findViewById(R.id.tuesdayToggle);
+                tues.setChecked(true);
+            }
+            if (days[2] == 1) {
+                ToggleButton wed = findViewById(R.id.wednesdayToggle);
+                wed.setChecked(true);
+            }
+            if (days[3] == 1) {
+                ToggleButton thurs = findViewById(R.id.thursdayToggle);
+                thurs.setChecked(true);
+            }
+            if (days[4] == 1) {
+                ToggleButton fri = findViewById(R.id.fridayToggle);
+                fri.setChecked(true);
+            }
+            if (days[5] == 1) {
+                ToggleButton sat = findViewById(R.id.saturdayToggle);
+                sat.setChecked(true);
+            }
+            if (days[6] == 1) {
+                ToggleButton sun = findViewById(R.id.sundayToggle);
+                sun.setChecked(true);
+            }
+        }
+        else {
+            ConstraintLayout creating = new ConstraintLayout(sample);
+        }
+        //firstAlarm = false;
     }
 }
